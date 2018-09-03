a2d00bea14a9e42696d1c4191d9370195b6ef9b1
ciazhar <ciazhar.id@gmail.com>
2018-09-03 12:38:38 +0700

    update location model

--- location.go
+++ location.go
@@ -3,6 +3,8 @@ package util
 type GeoJson struct {
 	Lat			float64	`json:"lat,omitempty" bson:"-"`
 	Long		float64	`json:"long,omitempty" bson:"-"`
+	X 			float64 `json:"x"`
+	Y 			float64 `json:"y"`
 	Type        string    `json:"-"`
 	Coordinates []float64 `json:"coordinates"`
 }
