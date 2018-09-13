db04002032dc995e444e813cbb708d1fb7588aa0
ciazhar <ciazhar.id@gmail.com>
2018-09-13 20:30:50 +0700

    add validator

---
+++ validator.go
@@ -0,0 +1,38 @@
+package util
+
+import (
+	"github.com/ciazhar/db"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type ErrMissingField string
+type ErrNotFound string
+type ErrAlreadyExist string
+
+func (e ErrMissingField) Error() string {
+	return string(e) + " is required"
+}
+
+func (e ErrNotFound) Error() string {
+	return string(e) + " is not found"
+}
+
+func (e ErrAlreadyExist) Error() string {
+	return string(e) + " is already exists"
+}
+
+func IsValueAlreadyExists(collection string, variable string, value interface{}) error {
+	count,_ := db.Mongo.C(collection).Find(bson.M{variable:value}).Count()
+	if count>=1{
+		return ErrAlreadyExist(collection)
+	}
+	return nil
+}
+
+func IsValueNotFound(collection string, variable string, value interface{}) error {
+	count,_ := db.Mongo.C(collection).Find(bson.M{variable:value}).Count()
+	if count==0{
+		return ErrNotFound(collection)
+	}
+	return nil
+}
