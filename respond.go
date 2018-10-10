d278e881b33e975cc0259c3ae595a23240f8b8cc
ciazhar <ciazhar.id@gmail.com>
2018-10-10 23:31:45 +0700

    add json method

--- respond.go
+++ respond.go
@@ -14,6 +14,17 @@ func RespondWithError(w http.ResponseWriter, code int, msg string) {
 	RespondWithJson(w, code, map[string]string{"error": msg})
 }
 
+func Json(w http.ResponseWriter, code int, message string ,data interface{}) {
+	payload := ResponseData{
+		Message:message,
+		Data:data,
+	}
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
 func RespondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 	response, _ := json.Marshal(payload)
 	w.Header().Set("Content-Type", "application/json")
